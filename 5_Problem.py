6dee7d5b662492ea800f23d3b75d448c6e8fe51f
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-05 16:09:33 +0500

    Converting List into Tuple & Vise Versa

---
+++ 5_Problem.py
@@ -0,0 +1,18 @@
+# Converting List into Tuple
+
+my_list = [1, 2, 3, 4, 5]
+my_tuple = tuple(my_list)
+
+print("List:", my_list)
+print("Converted to Tuple:", my_tuple)
+
+
+
+
+# Converting Tuple into List
+
+my_tuple = (10, 20, 30, 40, 50)
+my_list = list(my_tuple)
+
+print("Tuple:", my_tuple)
+print("Converted to List:", my_list)
